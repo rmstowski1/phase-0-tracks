9c6dd0102daf007e1ab8fe6d9b869b3556dd6437
Russell Mstowski <russellmstowski@Russells-MacBook-Pro.local>
2016-08-22 03:49:46 -0500

    designed puppy class cd
    git
    g
    reset

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+/
+   Species ------
+   German Husky
+
+   Characteristics ------
+   Height: varies
+   Weight: varies
+   Color: White-Brown mix
+   Smart: Insanely smart
+   Friendly: yes
+
+   Behavior ------
+   Run
+   Jump
+   Sleep
+   Bark
+*/
+
