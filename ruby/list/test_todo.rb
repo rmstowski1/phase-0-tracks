26c03d87e6231bef8501bc654be0b5c3935a9825
Russell Mstowski <russellmstowski@Russells-MacBook-Pro.local>
2016-08-26 06:05:42 -0500

    need to finish

---
+++ ruby/list/test_todo.rb
@@ -0,0 +1,20 @@
+class TodoList
+	def initialize(chore_arr)
+		@chore_arr = chore_arr
+	end
+
+	def get_items()
+		puts chore_arr
+	end
+
+	def add_item(item)
+	end
+
+	def delete_item(item)
+
+	end
+
+	def get_item(x)
+	end
+end
+
--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,21 +1,25 @@
+require_relative 'test_todo'
+
 describe TodoList do
-  let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
+  let(:list) { TodoList.new("do the dishes", "mow the lawn") }
 
   it "stores the list items given on initialization" do
+    list.get_items
     expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
   end
 
   it "adds an item to the list" do
     list.add_item("mop")
-    expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
+    expect(list.add_item).to eq ["do the dishes", "mow the lawn", "mop"]
   end
 
   it "deletes an item" do
     list.delete_item("do the dishes")
-    expect(list.get_items).to eq ["mow the lawn"]
+    expect(list.delete_item).to eq ["mow the lawn"]
   end
 
   it "retrieves an item by index" do
-    expect(list.get_item(0)).to eq "do the dishes"
+    list.get_item(0)
+    expect(list.get_item(0)).to eq ["do the dishes"]
   end
 end
