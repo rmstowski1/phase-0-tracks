d3841c70693d53de114a45977e842ad0190ec9a9
Russell Mstowski <russellmstowski@Russells-MacBook-Pro.local>
2016-08-28 16:43:41 -0500

    created object with two arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,23 @@
 var colors = ["blue", "green", "black", "purple"];
 
 var horses = ["Ed", "George", "Lex", "Liz"];
+
+colors.push("yellow");
+
+horses.push("Carl");
+
+var colored_horses = {};
+if (horses.length == colors.length){
+		for(var i = 0; i <= horses.length; i++){
+		colored_horses[horses[i]] = colors[i];
+	}
+}
+
+console.log(horses);
+console.log("");
+
+console.log(colors);
+console.log("");
+
+console.log(colored_horses["Ed"]);
+console.log(colored_horses["Lex"]);
