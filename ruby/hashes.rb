db49377200852309477c22d5ae32c8d0ce9a4e89
Russell Mstowski <russellmstowski@Russells-MacBook-Pro.local>
2016-09-06 00:20:29 -0500

    fixed errors

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,19 +1,40 @@
-/  - create a hash
-	- allow for user input to find out information
-	- convert data types if needed
+/  
+   - Gather user info and convert data types if needed
+    - create a hash
+	- store information
 	- print out filled hash
 	- let user update a key
    - print updated hash
 */
+puts "Name: "
+full_name = gets.chomp
+
+puts "Age: "
+client_age = gets.to_i
+
+puts "Number of children: "
+num_of_chil = gets.to_i
+
+puts "Decor Theme: "
+decor = gets.chomp
+
+puts "Favorite color blue(y/n): "
+blue = gets.match(/y/) != nil
+
+puts "Favorite color red(y/n): "
+red = gets.match(/y/) != nil
+
+puts "Favorite color green(y/n): "
+green = gets.match(/y/) != nil
 
 applicant = { 
-	name: gets.chomp.to_s,
-	age: gets.chomp.to_i,
-	number_of_children: gets.chomp.to_i,
-	decor_theme: gets.chomp.to_s,
-	fav_color_blue: gets.chomp.match(/true/) != nil,
-	fav_color_red: gets.chomp.match(/true/) != nil,
-	fav_color_green: gets.chomp.match(/true/) != nil
+	name: full_name,
+	age: client_age,
+	number_of_children: num_of_chil,
+	decor_theme: decor,
+	fav_color_blue: blue,
+	fav_color_red: red,
+	fav_color_green: green
 }
 
 p applicant
@@ -26,7 +47,7 @@ if user_input == "none"
 else
 	if user_input == "name" || user_input == "decor_theme"
 	user_input = user_input.to_sym
-	applicant[user_input] = gets.chomp.to_s
+	applicant[user_input] = gets.chomp
 	elsif user_input == "age" || user_input == "number_of_children"
 	user_input = user_input.to_sym
 	applicant[user_input] = gets.chomp.to_i
